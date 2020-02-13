780a11061d78bd583b44af9141e206b09d61c661
Sebastien Jurkowski <sebastienjurkowski@gmail.com>
2020-02-13 17:37:03 +0800

    added stardesign scss styling

--- react-foodiboo/src/components/stardesign.js
+++ react-foodiboo/src/components/stardesign.js
@@ -17,47 +17,54 @@ const stardesign =  ({
       };
     
     return (
-        <svg height={height} width={width} viewBox="0 0 250 500"  >
-        <polyline stroke-linejoin="round"
+        <svg height={height} width={width} viewBox="50 0 250 500"  >
+        <polyline 
+        className="polyline-1"
         points={`100,100 150,100 125,0 100,100 `}
-        style={{ fill: "white", stroke: "black", strokeWidth: 3 }}
+        // style={{  stroke: "black", strokeWidth: 3 }}
       />
        <polyline 
         points={`100,100 150,100 125,${getY(score1)} 100,100 `}
-        style={{ fill: "yellow", stroke: "blue", strokeWidth: 3 }}
+        className="polyline-2"
+        // style={{  stroke: "blue", strokeWidth: 3 }}
       />
        <polyline transform="rotate(72 125 134)"
         points={`100,100 150,100 125,0 100,100 `}
-        style={{ fill: "white", stroke: "black", strokeWidth: 3 }}
+        className="polyline-1"
+        
       />
       <polyline transform="rotate(72 125 134)"
         points={`100,100 150,100 125,${getY(score2)} 100,100 `}
-        style={{ fill: "yellow", stroke: "blue", strokeWidth: 3 }}
+        className="polyline-2"
+        
       />
        <polyline transform="rotate(144 125 134)"
         points={`100,100 150,100 125,0 100,100 `}
-        style={{ fill: "white", stroke: "black", strokeWidth: 3 }}
+        className="polyline-1"
+        
       />
       <polyline transform="rotate(144 125 134)"
         points={`100,100 150,100 125,${getY(score3)} 100,100 `}
-        style={{ fill: "yellow", stroke: "blue", strokeWidth: 3 }}
-      />
+        className="polyline-2"
+        />
        <polyline transform="rotate(216 125 134)"
         points={`100,100 150,100 125,0 100,100 `}
-        style={{ fill: "white", stroke: "black", strokeWidth: 3 }}
+        className="polyline-1"
+        
       />
       <polyline transform="rotate(216 125 134)"
         points={`100,100 150,100 125,${getY(score4)} 100,100 `}
-        style={{ fill: "yellow", stroke: "blue", strokeWidth: 3 }}
+        className="polyline-2"
       />
        <polyline transform="rotate(288 125 134)"
         points={`100,100 150,100 125,0 100,100 `}
-        style={{ fill: "white", stroke: "black", strokeWidth: 3 }}
+        className="polyline-1"
+        
       />
       <polyline transform="rotate(288 125 134)"
         points={`100,100 150,100 125,${getY(score5)} 100,100 `}
-        style={{ fill: "yellow", stroke: "blue", strokeWidth: 3 }}
-      />
+        className="polyline-2"
+           />
       <polyline
         points={`100,100 150,100 165,147 125,176 85,147 100,100 `}
         style={{ fill: "yellow", stroke: "yellow", strokeWidth: 3 }}
